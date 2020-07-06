e9b6cf690d38a71a558dd62b04f9bcea55fd835a
Smith <smithwebdd@gmail.com>
2020-07-06 12:55:10 -0400

    Customized portfolio routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
-  resources :portfolios
+  #Set root path for homepage
   root to: 'pages#home'
+
+  #Adjusted routes to no longer show the controller name
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
+
+  #Resource routes
   resources :blogs
+  resources :portfolios, except: [:show]
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
