a81f962932283cdd86addd83b40c9bd0eeffd5fa
Smith <smithwebdd@gmail.com>
2020-07-08 03:01:25 -0400

    Implemented custom routes, added CRUD functions

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,5 @@
 class BlogsController < ApplicationController
-  before_action :set_blog, only: [:show, :edit, :update, :destroy]
+  before_action :set_blog, only: [:show, :edit, :update, :destroy, :toggle_status]
 
   # GET /blogs
   # GET /blogs.json
@@ -53,10 +53,17 @@ class BlogsController < ApplicationController
     @blog.destroy
     respond_to do |format|
       format.html { redirect_to blogs_url, notice: 'Blog was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
+  def toggle_status
+    if @blog.draft?
+      @blog.published!
+    elsif @blog.published?
+      @blog.draft!
+    end
+    redirect_to blogs_url, notice: 'Post status has been updated'
+  end
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_blog
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,13 @@ Rails.application.routes.draw do
   get 'contact', to: 'pages#contact'
 
   #Resource routes
-  resources :blogs
+  resources :blogs do
+    member do
+      #Allows the enum to toggle between draft and published status
+      get :toggle_status
+    end
+  end
+
   resources :portfolios, except: [:show]
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
