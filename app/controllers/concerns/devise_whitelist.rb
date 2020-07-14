49f52ac66156c152286a5e67636e9edac0ebadfb
Smith <smithwebdd@gmail.com>
2020-07-14 14:43:09 -0400

    Completed authentication with virtual attributes for user model

--- app/controllers/concerns/devise_whitelist.rb
+++ app/controllers/concerns/devise_whitelist.rb
@@ -4,6 +4,7 @@ module DeviseWhitelist
   included do
     before_action :configure_permitted_parameters, if: :devise_controller?
   end
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
