c51f45e463c76516ddf2a8c9d48856d79331a5e7
Smith <smithwebdd@gmail.com>
2020-07-24 17:52:13 -0400

    Fixed bug with login helper method by setting defaults

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  def login_helper style
+  def login_helper style = ''
      if current_user.is_a?(GuestUser)
        (link_to "Sign In", new_user_session_path, class: style) +
          ' '.html_safe +
