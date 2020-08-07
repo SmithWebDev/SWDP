071e1c53bbf3c41282e236a5d4e4083e410f0a99
Smith <smithwebdd@gmail.com>
2020-08-07 09:21:26 -0400

    Updated file upload forms and refactored placeholder functionality into view

--- app/helpers/portfolios_helper.rb
+++ app/helpers/portfolios_helper.rb
@@ -1,2 +1,19 @@
 module PortfoliosHelper
+  def image_generator(height:, width:)
+    "http://placehold.it/#{height}x#{width}"
+  end
+  def port_img img, type
+    if img.model.main_image? || img.model.thumb_image?
+      img
+    elsif type == 'thumb'
+      image_generator(height: '350', width: '200')
+    elsif type == 'main'
+      image_generator(height: '600', width: '400')
+    end
+  end
 end
+
+
+
+# self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
+# self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
