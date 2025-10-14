883a3fe94b9305764854e45d5cd4a2b447e63341
Darrick Smith <smithwebdd@gmail.com>
2025-10-14 10:02:22 -0400

    [chore] Updated code styling and ruby version in gemfile

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
-ruby '2.7.0'
+ruby '2.7.8'
 
 gem 'rails', '~> 5.2.4', '>= 5.2.4.3'
 gem 'pg', '>= 0.18', '< 2.0'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
-  #def after_sign_out_path_for(resource_or_scope)
+  # def after_sign_out_path_for(resource_or_scope)
   #  request.referrer
-  #end
+  # end
 
   def nav_items
     [
@@ -24,61 +24,61 @@ module ApplicationHelper
       {
         url: portfolios_path,
         title: 'Portfolio'
-      },
+      }
     ]
   end
 
-  def nav_helper style, tag_type
+  def nav_helper(style, tag_type)
     nav_links = ''
     nav_items.each do |item|
       nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
     end
-    #nav_links = <<NAV
-    #<#{tag_type}><a href="#{root_path}" class="#{style} #{active? root_path}">Home</a></#{tag_type}>
-    #<#{tag_type}><a href="#{about_path}" class="#{style} #{active? about_path}">About</a></#{tag_type}>
-    #<#{tag_type}><a href="#{contact_path}" class="#{style}  #{active? contact_path}">Contact</a></#{tag_type}>
-    #<#{tag_type}><a href="#{blogs_path}" class="#{style} #{active? blogs_path}">Blog</a></#{tag_type}>
-    #<#{tag_type}><a href="#{portfolios_path}" class="#{style} #{active? portfolios_path}">Portfolio</a></#{tag_type}>
-    #NAV
+    # nav_links = <<NAV
+    # <#{tag_type}><a href="#{root_path}" class="#{style} #{active? root_path}">Home</a></#{tag_type}>
+    # <#{tag_type}><a href="#{about_path}" class="#{style} #{active? about_path}">About</a></#{tag_type}>
+    # <#{tag_type}><a href="#{contact_path}" class="#{style}  #{active? contact_path}">Contact</a></#{tag_type}>
+    # <#{tag_type}><a href="#{blogs_path}" class="#{style} #{active? blogs_path}">Blog</a></#{tag_type}>
+    # <#{tag_type}><a href="#{portfolios_path}" class="#{style} #{active? portfolios_path}">Portfolio</a></#{tag_type}>
+    # NAV
     nav_links.html_safe
   end
 
   def alerts
-    alert = (flash[:alert] || flash[:error] || flash[:notice])
-    if alert
-      alert_generator alert
-    end
+    alert = flash[:alert] || flash[:error] || flash[:notice]
+    return unless alert
+
+    alert_generator alert
   end
 
-  def alert_generator msg
-    js add_gritter(msg, title: 'SWD' , sticky: false)
+  def alert_generator(msg)
+    js add_gritter(msg, title: 'SWD', sticky: false)
   end
 
-  def active? path
-    "active" if current_page? path
+  def active?(path)
+    'active' if current_page? path
   end
 
-  def login_helper style = ''
+  def login_helper(style = '')
     if current_user.is_a?(GuestUser)
-      (link_to "Sign In", new_user_session_path, class: style) +
+      (link_to 'Sign In', new_user_session_path, class: style) +
         ' '.html_safe +
-        (link_to "Register", new_user_registration_path, class: style)
+        (link_to 'Register', new_user_registration_path, class: style)
     else
-      link_to "Signout", destroy_user_session_path, class: style, method: :delete
-      #redirect_to root_path
+      link_to 'Signout', destroy_user_session_path, class: style, method: :delete
+      # redirect_to root_path
     end
   end
-  #after_sign_out_path_for()
+  # after_sign_out_path_for()
 
   def source_helper(styles)
-    if session[:source]
-      greeting = "Thanks for visiting me from #{session[:source]}, please feel free to #{ link_to 'contact me', contact_path } if you'd like to work together"
-      content_tag(:div, greeting.html_safe, class: styles)
-    end
+    return unless session[:source]
+
+    greeting = "Thanks for visiting me from #{session[:source]}, please feel free to #{link_to 'contact me',
+                                                                                               contact_path} if you'd like to work together"
+    content_tag(:div, greeting.html_safe, class: styles)
   end
 
   def copyright_generator
     SmithViewTool::Renderer.copyright 'SmithWebDev', 'All Rights Reserved'
   end
-
 end
